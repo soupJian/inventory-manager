04fe7cfa4fbc5f969a58c92acdebfa5cfc9bc596
Jian Tang <soupjian@163.com>
2022-08-29 18:24:20 -0700

    feat: new contacts 模块

--- src/sections/crm-hub/dashboard/new-contact.jsx
+++ src/sections/crm-hub/dashboard/new-contact.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Dropdown, Space, Menu } from 'antd'
+import { DownOutlined } from '@ant-design/icons'
 import styles from './index.module.scss'
-
+const list = [
+  { key: 1, text: 'This Week' },
+  { key: 2, text: 'Last week' },
+  { key: 3, text: 'This month' },
+  { key: 4, text: 'Last month' },
+  { key: 5, text: 'This season' },
+  { key: 6, text: 'This season' },
+  { key: 7, text: 'This Year' },
+  { key: 8, text: 'Last Year' }
+]
 const NewContact = () => {
+  const menu = (
+    <Menu
+      items={list.map((item) => {
+        return {
+          key: item.key,
+          label: (
+            <span onClick={() => handleChooseSelect(item)}>{item.text}</span>
+          )
+        }
+      })}
+    />
+  )
   return (
     <div className={styles['new-contacts']}>
       <Row>
         <Col className={styles.title}>New contacts</Col>
-        <Col></Col>
+        <Col>
+          <div className={styles.num}>153</div>
+          <Dropdown overlay={menu} className={styles.dropdown}>
+            <Space>
+              Hover me
+              <DownOutlined />
+            </Space>
+          </Dropdown>
+        </Col>
       </Row>
     </div>
   )
