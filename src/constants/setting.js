4e2f90a9456718aefe146b2bb93308fc7a271d6e
Jian Tang <soupjian@163.com>
2022-09-12 17:22:31 -0700

    feat: 新增实现随机颜色

---
+++ src/constants/setting.js
@@ -0,0 +1,12 @@
+export const pipelineColors = [
+  '#FF7B7B',
+  '#FABF66',
+  '#2C88DD',
+  '#77D755',
+  '#8781FF',
+  '#3BC7F3',
+  '#E4D54B',
+  '#6EB17D',
+  '#B7B7B7',
+  '#2EBEBD'
+]
--- src/sections/setting/pipeline/index.jsx
+++ src/sections/setting/pipeline/index.jsx
@@ -2,59 +2,14 @@ import React, { useState } from 'react'
 import { Row, Col, Select, message } from 'antd'
 // components -----------
 import PipelineSales from './components/pipeline-sales.jsx'
+import PipelineReverse from './components/pipeline-reverse'
+import PipelineSupport from './components/pipeline-support'
+
 import styles from './index.module.scss'
 //js =---------
 const { Option } = Select
 const Pipeline = () => {
   const [headerSelect, setHeaderSelect] = useState('Sales pipeline')
-  const [salesList, setSaleslist] = useState([
-    {
-      name: 'Interest showed',
-      color: '#FF7B7B'
-    },
-    {
-      name: 'Initial mockup',
-      color: 'rgba(247, 148, 0, 0.6)'
-    },
-    {
-      name: 'Mockup revising',
-      color: '#2C88DD'
-    },
-    {
-      name: 'Invoice sent',
-      color: '#77D755'
-    },
-    {
-      name: 'Closed won',
-      color: '#2EBEBD'
-    },
-    {
-      name: 'Closed lost',
-      color: '#B7B7B7'
-    }
-  ])
-  // 编辑 slaes pipeline
-  const updateSalesPipeline = (oldValue, newValue) => {
-    setSaleslist((list) => {
-      const index = salesList.findIndex((item) => item.name == oldValue)
-      const newList = [...list]
-      newList[index].name = newValue
-      return newList
-    })
-  }
-
-  // 创建 sale pipeline
-  const createSalesPipeline = (salesName, afterName) => {
-    setSaleslist((list) => {
-      const newList = [...list]
-      const index = newList.findIndex((item) => item.name == afterName)
-      newList.splice(index + 1, 0, {
-        name: salesName,
-        color: 'pink'
-      })
-      return newList
-    })
-  }
   return (
     <div className={styles.assiging}>
       {/* header-search */}
@@ -76,13 +31,9 @@ const Pipeline = () => {
         </Col>
       </Row>
       {/* container */}
-      {headerSelect == 'Sales pipeline' && (
-        <PipelineSales
-          salesList={salesList}
-          updateSalesPipeline={updateSalesPipeline}
-          createSalesPipeline={createSalesPipeline}
-        />
-      )}
+      {headerSelect == 'Sales pipeline' && <PipelineSales />}
+      {headerSelect == 'Support pipeline' && <PipelineSupport />}
+      {headerSelect == 'Reverse logistics' && <PipelineReverse />}
     </div>
   )
 }
